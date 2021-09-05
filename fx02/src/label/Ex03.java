f1512685229b475830d84f5558eea9db66595e7b
jungyooseok <wjddbtjr8888@naver.com>
2021-09-05 21:16:05 +0900

    fx02

---
+++ fx02/src/label/Ex03.java
@@ -0,0 +1,64 @@
+package label;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class Ex03 extends Application{
+	static int i=0;
+	@Override
+	public void start(Stage arg0) throws Exception {
+		HBox hbox = new HBox();
+		hbox.setPadding(new Insets(10));
+		
+		Label lbl = new Label("A label that needs to be wrapped");
+		/*
+		FlowPane fp = new FlowPane();
+		fp.getChildren().add(lbl);
+		fp.setPrefSize(100, 100);
+		*/
+		hbox.getChildren().add(lbl);
+		hbox.setPrefSize(100, 100);
+		
+		lbl.setWrapText(true);
+		lbl.setFont(new Font(20));
+		lbl.setRotate(48);
+		lbl.setTranslateY(50);
+
+		lbl.setOnMouseClicked(e->{
+			if(i==0) {
+				lbl.setRotate(0);
+				lbl.setTranslateY(0);
+				i=1;
+			}else {
+				lbl.setRotate(45);
+				lbl.setTranslateY(50);
+				i=0;
+			}
+		});
+		
+		
+		
+		arg0.setScene(new Scene(hbox));
+		arg0.show();
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
+
+
+
+
+
+
+
+
+
